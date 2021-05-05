c3601acf234502a96e90eb230c7f39a8e5fe31a9
Edeson Junior <edeson.jsbarbosa@hotmail.com>
2021-05-05 17:52:45 -0300

    Commit inicial

---
+++ EstruturaCondicionalComposta.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EstruturaCondicionalComposta {
+
+	public static void main(String[] args) {
+		
+		int hora;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Quantas horas?");
+		hora = sc.nextInt();
+		
+		if(hora < 12) {
+			System.out.println("Bom dia");
+		}
+		else {
+			System.out.println("Boa tarde");
+		}
+		
+		sc.close();
+		
+	}
+
+}
